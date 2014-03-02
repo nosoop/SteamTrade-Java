423e261301971d3ad090e7c713fef22d07087d9d
nosoop <nosoop@users.noreply.github.com>
2014-03-01 22:36:13 -0800

    Made TradeInternalCurrency a subclass of TradeInternalItem.

--- SteamTrade-Java/src/com/nosoop/steamtrade/inventory/TradeInternalCurrency.java
+++ SteamTrade-Java/src/com/nosoop/steamtrade/inventory/TradeInternalCurrency.java
@@ -11,31 +11,10 @@ import org.json.JSONObject;
  *
  * @author nosoop
  */
-public class TradeInternalCurrency {
-
-    public boolean isRenamed;
-    
-    /**
-     * The display name of the item. If the item was renamed (as it could be in
-     * TF2, it will be that name.
-     */
-    public String displayName;
-    
-    /**
-     * The name it would be grouped under in the Steam Community Market. Is
-     * blank if not in the Market.
-     */
-    public String marketName;
-    public long id;
-    public int appid;
-    public long contextid;
+public class TradeInternalCurrency extends TradeInternalItem {
 
     TradeInternalCurrency(long id, JSONObject rgDescriptionItem) throws JSONException {
-        marketName = rgDescriptionItem.getString("market_name");
-        displayName = rgDescriptionItem.getString("name");
-
-        this.id = id;
-
+        super(id, rgDescriptionItem);
     }
     // TODO Add method to return overridable name instead of using basic display.
 }
