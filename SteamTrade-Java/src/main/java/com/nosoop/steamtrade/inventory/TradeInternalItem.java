0f72e3e3d2653991e385a3391b9f2bb807260743
nosoop <nosoop@users.noreply.github.com>
2014-06-08 18:36:12 -0700

    Changed TradeInternalItem's getDisplayName behavior. By default, it now appends the type of the item in parentheses if it is a non-empty type.

--- SteamTrade-Java/src/main/java/com/nosoop/steamtrade/inventory/TradeInternalItem.java
+++ SteamTrade-Java/src/main/java/com/nosoop/steamtrade/inventory/TradeInternalItem.java
@@ -9,27 +9,34 @@ import bundled.steamtrade.org.json.JSONObject;
  * @author nosoop
  */
 public class TradeInternalItem extends TradeInternalAsset {
-
     public static final TradeInternalItem UNAVAILABLE = null;
     long instanceid;
     boolean stackable;
 
-    public TradeInternalItem(AppContextPair appContext, 
-            JSONObject rgInventoryItem, JSONObject rgDescriptionItem) 
+    public TradeInternalItem(AppContextPair appContext,
+            JSONObject rgInventoryItem, JSONObject rgDescriptionItem)
             throws JSONException {
         super(appContext, rgInventoryItem, rgDescriptionItem);
 
         this.instanceid =
                 Long.parseLong(rgDescriptionItem.optString("instanceid", "0"));
-        
+
         this.stackable = (amount > 1);
     }
 
     public long getInstanceid() {
         return instanceid;
     }
-    
+
     public boolean isStackable() {
         return stackable;
+    }
+
+    @Override
+    public String getDisplayName() {
+        if (getType().isEmpty()) {
+            return super.getDisplayName();
+        }
+        return String.format("%s (%s)", super.getDisplayName(), this.getType());
     }
 }
